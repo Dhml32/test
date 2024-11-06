f2d6f5a6a4aad1546df34df87bbf3c45d21f0282
Abdulmajeed4 <Abdulmaj77d@gmail.com>
2024-11-06 17:24:19 +0300

    gggg

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49eaa80e262f6503da095e79ad37fb024511abe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c7220df36613d8bbfbee13e273c6e0fd4616c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
